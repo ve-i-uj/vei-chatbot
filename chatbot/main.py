d9c506cbb263a03be4b61849d4afdff37e712d14
Aleksei Burov <burov_alexey@mail.ru>
2020-06-12 12:04:39 +0500

    Аргументы для запуска из командой строки

--- chatbot/main.py
+++ chatbot/main.py
@@ -2,25 +2,41 @@
 # -*- coding: utf-8 -*-
 """Входная точка для запуска бота."""
 
+import argparse
 import logging
 
 from chatbot.misc import log
 from chatbot import pizzaorder
 
-def main():
-    # TODO: (burov_alexey@mail.ru 11 июн. 2020 г. 19:59:30) 
-    # нужно логер выставлять в ERROR при cli, чтобы не фонил
 
-#     log.setup_root_logger('ERROR')
-    if True:
+def read_args():
+    """Прочитать аргументы командной стоки."""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--ui', dest='ui', type=str, choices=['cli', 'telegram'],
+                        help='User Interface of the bot')
+    parser.add_argument('--log-level-name', dest='log_level', type=str,
+                        choices=[n for n in logging._levelNames.values() if isinstance(n, str)],
+                        help='Logging level')
+    parser.set_defaults(log_level='ERROR',
+                        ui='cli')
+
+    return parser.parse_args()
+
+
+def main():
+    namespace = read_args()
+    
+    log.setup_root_logger(namespace.log_level)
+    if namespace.ui == 'cli':
         from chatbot.ui import cli
         ui = cli.CLI()
-        model = pizzaorder.PizzaOrderModel(
-            uid=1,
-            user_interface=ui
-        )
+        _model = pizzaorder.PizzaOrderModel(uid=1, user_interface=ui)
         ui.run()
-    
+    elif namespace.ui == 'telegram':
+        pass
+        
 
 if __name__ == '__main__':
     main()
